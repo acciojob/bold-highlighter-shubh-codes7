1e0eb96c4dcecdfdc4ef2fe048169b1417aacdb8
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-19 14:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
+
 function highlight() {
     //Write your code here
-
+	document.querySelectorAll("strong").forEach(elm => {
+		elm.style.color = "rgb(0, 128, 0)"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	document.querySelectorAll("strong").forEach(elm => {
+		elm.style.color = "rgb(0, 0, 0)"
+	})
     
 }
